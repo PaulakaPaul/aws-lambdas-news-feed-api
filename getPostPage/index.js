3b2f4fcee203cd6348870805c0ebd85880cad602
paulakapaul <p.e.iusztin@gmail.com>
2018-09-29 19:44:12 +0300

    timestamp getPost

--- getPostPage/index.js
+++ getPostPage/index.js
@@ -6,8 +6,10 @@ const docClient = new AWS.DynamoDB.DocumentClient({region: s.REGION});
 exports.handler= function(e, ctx, callback){
 
     let pageNumber = e.pageNumber;
+    let referenceTimestamp = e.referenceTimestamp
     
     console.log(pageNumber)
+    console.log(referenceTimestamp)
 
     if(f.isAnyNullOrEmpty(pageNumber)) {
         callback(null, 
@@ -30,11 +32,31 @@ exports.handler= function(e, ctx, callback){
 			if(err){
 				callback(null, f.createResponse('', err, '', 500));
 			} else {
-			    let startingPointInterval = pageNumber * s.PAGE_SIZE;
-			    let endPointInterval = startingPointInterval + s.PAGE_SIZE;
-			    let items = data['Items'].reverse();
-			    items = items.slice(startingPointInterval, endPointInterval);
-				callback(null, f.createResponse(items, '', '', 200));
+
+                if(referenceTimestamp === undefined) {
+                    let startingPointInterval = pageNumber * s.PAGE_SIZE;
+                    let endPointInterval = startingPointInterval + s.PAGE_SIZE;
+                    let items = data['Items'].reverse();
+                    items = items.slice(startingPointInterval, endPointInterval);
+                    callback(null, f.createResponse(items, '', '', 200));
+                } else {
+                    let items = data['Items'];
+                    // endPointInterval because the items are in ascending order and we will want them in
+                    // descending order. But it's better to reverse only the slice of items and not the whole array.
+                    let endPointInterval = items.map(function(p) {return p.postTStamp }).indexOf(referenceTimestamp);
+                    console.log('endPointInterval: ' + endPointInterval)
+
+                    if(endPointInterval === -1) {
+                        // If the item does not exist stop the logic.
+                        callback(null, f.createResponse([], '', '', 200));
+                    } else { 
+                        // Send the page of items since the index of the sent item - 1.
+                        endPointInterval -= 1;
+                        let startingPointInterval = endPointInterval - s.PAGE_SIZE;
+                        items = items.slice(startingPointInterval, endPointInterval).reverse();
+                        callback(null, f.createResponse(items, '', '', 200));
+                    }
+                }
 			}
 		});
     }
